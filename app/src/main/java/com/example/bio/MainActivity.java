a985daa6d5cb80bd42b1e4c2ceb0c4a64b074640
Deepak <souparnikad40@gmail.com>
2021-04-18 12:48:00 +0530

    bio app finished and hide keyboard automatically is added

--- app/src/main/java/com/example/bio/MainActivity.java
+++ app/src/main/java/com/example/bio/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.bio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText enterhobbies;
+    private TextView hobbies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        enterhobbies = findViewById(R.id.hobbies);
+        hobbies = findViewById(R.id.hobbies_text);
+    }
+
+    public void add_hobbies(View view) {
+        hobbies.setText(String.format("Hobbies: %s ", enterhobbies.getText().toString().trim()));
+        hobbies.setVisibility(View.VISIBLE);
+
+
+        //hide keyboard
+        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
     }
 }
